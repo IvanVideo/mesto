19ec86f230f451f906d519cae1374dd1fc38c994
IvanVideo <72228147+IvanVideo@users.noreply.github.com>
2020-11-22 21:24:59 +0300

    1_review

--- script/script.js
+++ script/script.js
@@ -11,23 +11,22 @@ let profileAbout = document.querySelector('.profile__subtitle');
 profileButton.addEventListener('click', handleProfileButtonClik)
 profileButtonClose.addEventListener('click', handleProfileButtonCloseClik)
 popupButtonSave.addEventListener('click', formSubmitHandler)
-popupButtonSave.addEventListener('submit', formSubmitHandler)
 
 
 function handleProfileButtonClik() {
-    popup.classList.add('popup_popup_opened');
-    popupNameForm.value = document.querySelector('.profile__title').textContent;
-    popupAboutForm.value = document.querySelector('.profile__subtitle').textContent;
+    popup.classList.add('popup_opened');
+    popupNameForm.value = profileName.textContent;
+    popupAboutForm.value = profileAbout.textContent;
 }
 
 function handleProfileButtonCloseClik() {
-    popupClose.classList.remove('popup_popup_opened');
+    popupClose.classList.remove('popup_opened');
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
 
-    popup.classList.remove('popup_popup_opened');
-    profileName.textContent = document.querySelector('.popup__name').value;
-    profileAbout.textContent = document.querySelector('.popup__about').value;
+    popup.classList.remove('popup_opened');
+    profileName.textContent = popupNameForm.value;
+    profileAbout.textContent = popupAboutForm.value;
 }
