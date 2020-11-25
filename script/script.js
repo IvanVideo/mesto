100b12b508567fe56e50f6c523f3824a800c695c
IvanVideo <72228147+IvanVideo@users.noreply.github.com>
2020-11-25 16:02:57 +0300

    2_review

--- script/script.js
+++ script/script.js
@@ -1,17 +1,17 @@
 let profileButton = document.querySelector('.profile__link');
 let popup = document.querySelector('.popup');
 let profileButtonClose = document.querySelector('.popup__close');
-let popupClose = document.querySelector('.popup');
 let popupNameForm = document.querySelector('.popup__name');
 let popupAboutForm = document.querySelector('.popup__about');
 let popupButtonSave = document.querySelector('.popup__button');
 let profileName = document.querySelector('.profile__title');
 let profileAbout = document.querySelector('.profile__subtitle');
+let popupForm = document.querySelector('.popup__form');
 
 profileButton.addEventListener('click', handleProfileButtonClik)
 profileButtonClose.addEventListener('click', handleProfileButtonCloseClik)
 popupButtonSave.addEventListener('click', formSubmitHandler)
-
+popupForm.addEventListener('submit', formSubmitHandler)
 
 function handleProfileButtonClik() {
     popup.classList.add('popup_opened');
@@ -20,13 +20,43 @@ function handleProfileButtonClik() {
 }
 
 function handleProfileButtonCloseClik() {
-    popupClose.classList.remove('popup_opened');
+    popup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-
-    popup.classList.remove('popup_opened');
     profileName.textContent = popupNameForm.value;
     profileAbout.textContent = popupAboutForm.value;
+    handleProfileButtonCloseClik() 
 }
+
+// let profileButton = document.querySelector('.profile__link');
+// let popup = document.querySelector('.popup');
+// let profileButtonClose = document.querySelector('.popup__close');
+// let popupNameForm = document.querySelector('.popup__name');
+// let popupAboutForm = document.querySelector('.popup__about');
+// let popupButtonSave = document.querySelector('.popup__button');
+// let profileName = document.querySelector('.profile__title');
+// let profileAbout = document.querySelector('.profile__subtitle');
+
+// profileButton.addEventListener('click', handleProfileButtonClik)
+// profileButtonClose.addEventListener('click', handleProfileButtonCloseClik)
+// popupButtonSave.addEventListener('click', formSubmitHandler)
+
+// function handleProfileButtonClik() {
+//     popup.classList.add('popup_opened');
+//     popupNameForm.value = profileName.textContent;
+//     popupAboutForm.value = profileAbout.textContent;
+// }
+
+// function handleProfileButtonCloseClik() {
+//     popup.classList.remove('popup_opened');
+// }
+
+// function formSubmitHandler (evt) {
+//     evt.preventDefault();
+
+//     popup.classList.remove('popup_opened');
+//     profileName.textContent = popupNameForm.value;
+//     profileAbout.textContent = popupAboutForm.value;
+// }
