808e186aebeb1621cb971a8a1e5817b094f155e9
IvanVideo <72228147+IvanVideo@users.noreply.github.com>
2020-11-26 17:19:26 +0300

    review_last

--- script/script.js
+++ script/script.js
@@ -3,7 +3,6 @@ let popup = document.querySelector('.popup');
 let profileButtonClose = document.querySelector('.popup__close');
 let popupNameForm = document.querySelector('.popup__name');
 let popupAboutForm = document.querySelector('.popup__about');
-let popupButtonSave = document.querySelector('.popup__button');
 let profileName = document.querySelector('.profile__title');
 let profileAbout = document.querySelector('.profile__subtitle');
 let popupForm = document.querySelector('.popup__form');
